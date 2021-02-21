0a4ae6cdffa3ba93fb8a405503c9ecce6ebcd620
JamesOwers <james.f.owers+github@gmail.com>
2021-02-21 12:39:51 +0000

    Install rebiber

--- install_linux.sh
+++ install_linux.sh
@@ -18,4 +18,5 @@ sudo tlmgr update l3backend
 # Would it be simpler to just update all packages? (takes ~10m)
 # sudo tlmgr update --all
 pip install pandoc-fignos pandoc-eqnos pandoc-tablenos pandoc-secnos pandoc-shortcaption
-
+pip install rebiber -U
+rebiber --update
